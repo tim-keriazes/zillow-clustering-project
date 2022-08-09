e827fea3d9a910c660b021399fb43796b8c50631
tim-keriazes <105242897+tim-keriazes@users.noreply.github.com>
2022-08-09 05:53:44 -0700

    Add files via upload

---
+++ acquire.py
@@ -0,0 +1,88 @@
+#from acquire import *
+
+import pandas as pd
+import numpy as np
+import os
+from env import host, user, password
+
+#function uses info from env.py file to create a connection url to access db
+
+def get_connection(db, user=user, host=host, password=password):
+    
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+##reads in telco data from db, writed data to csv in no local file exists, returns a df
+def get_telco_data():
+    
+    if os.path.isfile('telco.csv'):
+        #if csv exists read in data
+        df = pd.read_csv('telco.csv', index_col=0)
+        
+    else:
+        #read in fresh data from db to df
+        df = new_telco_data()
+        #cache data
+        df.to_csv('telco.csv')
+        
+    return df
+
+def get_zillow_data():
+    """Seeks to read the cached zillow.csv first """
+    filename = "zillow.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        return get_new_zillow_data()
+
+def get_new_zillow_data():
+    """Returns a dataframe of all 2017 properties that are Single Family Residential"""
+
+    sql = """
+    select 
+    bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+    from properties_2017
+    join propertylandusetype using (propertylandusetypeid)
+    where propertylandusedesc = "Single Family Residential"
+    """
+    return pd.read_sql(sql, get_db_url("zillow"))
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+sql_query = '''
+        SELECT prop.*,
+        predictions_2017.logerror,
+        predictions_2017.transactiondate,
+        air.airconditioningdesc,
+        arch.architecturalstyledesc,
+        build.buildingclassdesc,
+        heat.heatingorsystemdesc,
+        land.propertylandusedesc,
+        story.storydesc,
+        type.typeconstructiondesc
+        FROM properties_2017 prop
+        JOIN (
+            SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+            FROM predictions_2017
+            GROUP BY parcelid
+            ) pred USING(parcelid)
+        JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                          AND pred.max_transactiondate = predictions_2017.transactiondate
+        LEFT JOIN airconditioningtype air USING(airconditioningtypeid)
+        LEFT JOIN architecturalstyletype arch USING(architecturalstyletypeid)
+        LEFT JOIN buildingclasstype build USING(buildingclasstypeid)
+        LEFT JOIN heatingorsystemtype heat USING(heatingorsystemtypeid)
+        LEFT JOIN propertylandusetype land USING(propertylandusetypeid)
+        LEFT JOIN storytype story USING(storytypeid)
+        LEFT JOIN typeconstructiontype type USING(typeconstructiontypeid)
+        WHERE propertylandusedesc = "Single Family Residential"
+            AND transactiondate <= '2017-12-31'
+            AND prop.longitude IS NOT NULL
+            AND prop.latitude IS NOT NULL
+        '''
+    
+def get_zillow_data():
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    df = df.drop(columns='id')
+    return df
